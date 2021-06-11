6cdef550156b0eecd700e2f74ff7dc736ddb0204
SantiagoLimaCoimbra <santiagolimacoimbra@gmail.com>
2021-06-11 18:15:15 -0300

    Base do projeto pronta.

---
+++ src/test/java/br/com/galeriaSA/GaleriaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.galeriaSA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GaleriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
