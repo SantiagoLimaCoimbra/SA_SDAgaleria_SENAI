970e29978da77b89d51eaba9725921f9e39642f6
SantiagoLimaCoimbra <santiagolimacoimbra@gmail.com>
2021-06-21 18:57:42 -0300

    Atualização:21/06 - Atualização do layout, agora com 2 index, o segundo parece mais promissor.

--- src/main/resources/static/js/scriptIndex.js
+++ src/main/resources/static/js/scriptIndex.js
@@ -1,3 +1,9 @@
-function show() {
-    document.getElementById('sidebar').classList.toggle('active');
+function openNav() {
+  document.getElementById("mySidebar").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidebar").style.width = "0";
+  document.getElementById("main").style.marginLeft= "0";
 }
